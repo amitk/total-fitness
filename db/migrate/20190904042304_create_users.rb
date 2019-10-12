9b1604ca0bb09d0e994f06280175ccb8849b7ee9
Amit Kumar <amit@joshsoftware.com>
2019-10-12 14:14:39 +0530

    changes
      + remove dob from user

--- db/migrate/20190904042304_create_users.rb
+++ db/migrate/20190904042304_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :gender, null: false
       t.integer :mobile, null: false
       t.string :email, null: false
-      t.string :auth_token
-      t.date :dob, null: false
+      # t.string :auth_token
+      # t.date :dob, null: false
       t.string :auth_token, null: false
       t.timestamps
     end
