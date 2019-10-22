f4fea83381891cbff5da6af8b24039234a6f45ce
Amit Kumar <amit@joshsoftware.com>
2019-10-22 22:27:42 +0530

    Add factory bot rails

--- spec/acceptance/api/v1/users_spec.rb
+++ spec/acceptance/api/v1/users_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 resource 'Users' do
+  let!(:user) { create :user }
   get '/api/v1/users' do
     example 'listing users' do
       do_request
       expect(status).to eq 200
+    end
+  end
+
+  get '/api/v1/users/:id' do
+    parameter :id, 'user id'
+    let(:id) { user.id }
+    example 'get one user' do
+      do_request
+      expect(status).to eq(200)
     end
   end
 end
