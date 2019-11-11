99643d6a1d137ee54d0183e795d3ca7bf4cfb4b5
Amit Kumar <amit@joshsoftware.com>
2019-11-11 21:03:54 +0530

    expose headers in cors gem

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module TotalFitness
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options], expose: ['X-USER-AUTH-TOKEN', 'X-USER-JWT-TOKEN']
       end
     end
   end
